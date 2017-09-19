d135dfda97badecae5a544ea276cfc1641a8c6d7
Max Harms <raelifin@gmail.com>
2017-09-18 17:37:15 -0700

    Minimal Board, Squares, and Players

---
+++ main.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+class Player(Enum):
+    RED = -1
+    BLACK = 1
+
+    def __str__(self):
+        return self.name.title()
+
+class Square(Enum):
+    EMPTY = 0
+    RED = -1
+    BLACK = 1
+
+    def __str__(self):
+        if self == self.RED:
+            return "X"
+        elif self == self.BLACK:
+            return "O"
+        else:
+            return " "
+
+def startingSquare(x, y):
+    """Returns the contents of a given square for the starting board state"""
+    if (x + y) % 2 == 0:
+        return Square.EMPTY
+    elif y < 3:
+        return Square.BLACK
+    elif y > 4:
+        return Square.RED
+    else:
+        return Square.EMPTY
+
+def showBoard(board):
+    for y in range(len(board)):
+        row = board[y]
+        print("{}|{}".format(y + 1, "".join([str(square) for square in row])))
+    print("  hgfedcba")
+
+startingBoard = [[startingSquare(x, y) for x in range(8)] for y in range(8)]
+activePlayer = Player.RED
+
+showBoard(startingBoard)
+print()
+print("{}'s turn".format(activePlayer))
